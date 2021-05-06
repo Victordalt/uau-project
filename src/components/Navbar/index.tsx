aac96575dd4834010c0c54815425794689ddbcec
Victor Teixeira <victordalt@hotmail.com>
2021-05-06 11:28:49 -0400

    Marcas fixed
    Marcas fixed
    Favicon switched

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import React,{useState, useEffect} from 'react';
 import * as Scroll from 'react-scroll';
 
-
+// React-scroll variables
 let Events    = Scroll.Events;
 let scrollSpy = Scroll.scrollSpy;
 var Link      = Scroll.Link;

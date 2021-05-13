ff56084e3504331434f9ffe279d9c40bb3bfae42
Victor Teixeira <victordalt@hotmail.com>
2021-05-12 22:24:38 -0400

    Form updated
    Form now sends email directly to the company.

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,33 +1,93 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import emailjs, { send } from 'emailjs-com';
 
 
 
 
 export default function Contato() {
+    const {register, handleSubmit, formState: {errors}, reset} = useForm<Inputs>();
+    const onError = (errors, e) => console.log(errors, e);
 
-    function confirmationPopup() {
-        var popup = document.getElementById("myPopup");
-        popup.classList.toggle("show");
-    }
+        
+    type Inputs = {
+        name: string;
+        subject: string;
+        email: string;
+        message: string;
+        
+      }
 
+      function onSubmitForm(values) {
+        console.log(values); 
+      }
+
+      
+
+      function sendEmail(e) {        
+        
+        // e.preventDefault();                
+
+        emailjs.sendForm('service_90x4ir9', 'template_nvozbm8', '#myForm', 'user_bdFMwqxwlNBeWtHydnzCp')
+        reset();
+        // .then((result) => {
+        //     console.log(result.text);
+        // }).catch((error) => {
+        //     console.log(error.text);
+        // });
+        // e.target.reset();        
+      }
+     
+      
 
     return (
         <div className={styles.contatoContainer}>
-            <h2>Fale conosco!</h2>
-            <form action="mailto:victordalt@hotmail.com" method="POST" > 
+            <h2>Fale conosco!</h2>         
+            <form onSubmit={handleSubmit(sendEmail)} id="myForm">
                 <div className={styles.firstRow}>
-                    <input type="text" placeholder="Seu Nome" className="name" name="name"/>
-                    <input type="text" placeholder="Seu E-mail" className="email" name="email"/>
+                    <div className={styles.nameWrapper}>
+                        <input type="text"  placeholder="Seu Nome" className="name" name="name" {...register("name", {required: "Por favor, digite seu nome." })}/>
+
+                        <span className={styles.errorMessage}>{errors?.name?.message}</span>
+                    </div>
+                    <div className={styles.emailWrapper}>
+                        <input type="text" placeholder="Seu E-mail" className="email" name="email" 
+                            {...register("email", 
+                                {   required: "Por favor, digite seu email.",
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                        message: "Por favor, digite um e-mail valido."
+                                    }
+                                 })}/>
+                        <span className={styles.errorMessage}>{errors?.email?.message}</span>
+                    </div>
                 </div>  
                 <div className={styles.secondRow}>
                     <ul>
-                        <li><input type="text" placeholder="Assunto" className="subject" name="subject"/></li>
-                        <li><textarea placeholder="Mensagem" className="message" name="message"/></li>                        
-                    </ul>          
-                </div>              
-                <input type="submit" onClick={confirmationPopup} value="Enviar" />
-                <span className={styles.popuptext} id="myPopup">Popup text...</span>
+                        <li><input type="text" placeholder="Assunto" className="subject" name="subject" {...register("subject", {required: "Por favor, digite o assunto." })}/></li>
+                        <span className={styles.errorMessage}>{errors?.subject?.message}</span>
+                        <li>
+                            <textarea placeholder="Mensagem" className="message" name="message" 
+                                {...register("message",
+                                        {   required: "Por favor, digite sua mensagem.",
+
+                                            minLength : {
+                                            value: 10, 
+                                            message: "Sua mensagem deve contar mais de 10 caracteres."
+                                            }
+                                        ,
+                                            maxLength : {
+                                            value: 500, 
+                                            message: "Sua mensagem deve contar menos de 500 caracteres."
+                                        }                            
+                                })}/>
+                        </li>
+                        <span className={styles.errorMessage}>{errors?.message?.message}</span>                        
+                    </ul>       
+                    <input type="submit" value="Enviar" />                                      
+                </div>                    
+                           
             </form>
         </div>
     )
--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
 import styles from "./styles.module.scss";
 
 export default function Description() {
     return (
         <div>
-            
             <div className={styles.bgContainer} >
-                            
                 <div className={styles.mainContainer}>
-                    
                     <h2>Apaixonados pela Criação e Gestão de novas marcas!</h2>
                     <p>Holding especializada na criação e gestão de novos negócios gastronômicos.</p>
                     <p>Apaixonados pela criação de novas marcas, hoje, a Uau Company gerencia 7 marcas próprias e uma marca da Uber Eats (Caseirinho ®.). Com aproximadamente 5000 pedidos mensais, a Uau Company busca atingir a meta de 10.000 pedidos mensais até o Final do ano. Qualidade e Dedicação são as palavras-chave que norteiam a holding. </p>
